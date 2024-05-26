f1eb25262d752b23929a5bcfb006c1d4c21e8571
ᴹᴿ᭄ Fʀᴀɴᴋ ᴏғᴄ 々 <darrelmucheri@gmail.com>
2024-05-26 11:49:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,18 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233508488192', 'Abraham Dwamena3', true]
+  ['263719647303', 'M', true], 
+  ['263719647303', 'R ', true],
+  ['263719647303', 'F', true], 
+  ['263719647303', 'R', true],
+  ['263719647303', 'A', true],
+  ['263719647303', 'N', true]
+  ['263719647303', 'K', true]
 ] //Number of owners
 
-global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.mods = ['263719647303'] 
+global.prems = ['263719647303']
+global.allowed = ['263719647303']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,20 +51,20 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '𝚂𝚄𝙱𝚉𝙴𝚁𝙾-𝙼𝙳'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '𝚂𝚄𝙱𝚉𝙴𝚁𝙾-𝚂𝚄𝙿𝚁𝙴𝙼𝙰𝙲𝚈 𝙱𝚈 :' 
+global.author = '𝙼𝚁 𝙵𝚁𝙰𝙽𝙺' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
+global.fgsc = 'https://github.com/MrFr2nk/ShitX' 
+global.fgyt = 'https://youtube.com/@mrfr4nk'
+global.fgpyp = 'https://youtube.com/@mrfr4nk'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
-global.wait = '*🕣 _XLICON IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 SubZero IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
